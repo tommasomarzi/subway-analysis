c58ed2a5e37e5c33758db6e81739e6592f3c249d
Tommaso Marzi <marzitommaso@gmail.com>
2021-12-17 10:33:22 +0100

    Improved plot

--- utilities.py
+++ utilities.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 ##--------------------FLOW DISTRIBUTION-------------------##
 ##--------------------------------------------------------##
 
-def flow_histogram(matrix, flow_type = None, n_bins = None, norm = False):
+def flow_histogram(matrix, flow_type = None, path = None, n_bins = None, save = False):
     """
     Build the histogram and plot it through a scatter plot.
     """
@@ -40,13 +40,28 @@ def flow_histogram(matrix, flow_type = None, n_bins = None, norm = False):
         plt.xlabel('$f_{i->.}$')
         plt.ylabel('P($f_{i->.}$)') 
         plt.xlim([10,1000000])   
-        plt.ylim([1e-7,1])  
+        plt.ylim([1e-7,1])    
+    elif flow_type == 'F15':       
+        plt.xlabel('$f_{i->j}$')
+        plt.ylabel('P($f_{i->j}$)') 
+        plt.xlim([10,10000])   
+        plt.ylim([1e-6,1])  
+    elif flow_type == 'U15':
+        plt.xlabel('$u_{i->0}$')
+        plt.ylabel('P($u_{i->0}$)')
+        plt.xlim([10,10000])
+        plt.ylim([1e-5,1])
+    elif flow_type == 'V15':
+        plt.xlabel('$v_{0->i}$')
+        plt.ylabel('P($v_{0->i}$)')
+        plt.xlim([10,10000])
+        plt.ylim([1e-5,1])
     
-    if norm:
-        n = n/sum(n)
-
+    n = n/sum(n)
     plt.xscale('log')
     plt.yscale('log')
-    plt.scatter(bins[:-1]+ 0.5*(bins[1:] - bins[:-1]), n, marker='.', c='red', s=40, alpha=0.6)
+    plt.scatter(bins[:-1]+ 0.5*(bins[1:] - bins[:-1]), n, marker='.', c='red', s=40, alpha=0.3)
     plt.grid()
+    if save:
+        plt.savefig(path + '/flow_distribution_'+str(flow_type)+'.png', dpi=1200)
     plt.show()
